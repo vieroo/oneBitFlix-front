eb2f98bcb753d6f0f2152bacd1d2ca6bfa7fa182
Eduardo Viero <duduviero@gmail.com>
2024-04-05 20:35:42 -0300

    Criacao dos slides, componente pai e filho. Conexao com o backend, inicio do visual dos slides

---
+++ src/components/common/slideComponent/index.tsx
@@ -0,0 +1,32 @@
+//@ts-ignore
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import { CourseType } from '@/src/services/courseService'
+import "@splidejs/splide/dist/css/splide.min.css"
+import SlideCard from '../slideCard'
+
+interface props {
+  course: CourseType[]
+}
+
+const slideComponent = ({ course }: props) => {
+  return (
+    <>
+      <div>
+        <Splide options={{
+          type: "loop",
+          perPage: 4,
+          perMove: 1,
+          pagination: false
+        }}>
+          {course?.map((course) => (
+            <SplideSlide key={course.id}>
+              <SlideCard course={course} />
+            </SplideSlide>
+          ))}
+        </Splide>
+      </div>
+    </>
+  )
+}
+
+export default slideComponent
