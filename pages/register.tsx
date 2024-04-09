b8923051292086a68f54996d21ce6172f8249120
Eduardo Viero <duduviero@gmail.com>
2024-04-08 22:56:14 -0300

    Conexao com o backend e criação do toast

--- pages/register.tsx
+++ pages/register.tsx
@@ -3,8 +3,50 @@ import styles from '../styles/registerLogin.module.scss';
 import Head from 'next/head'
 import { Container, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import Footer from '@/src/components/common/footer/Footer';
+import { FormEvent, useState } from 'react';
+import authService from '@/src/services/authService';
+import { useRouter } from 'next/router';
+import ToastComponent from '@/src/components/common/toast';
 
 const Register = () => {
+  const router = useRouter()
+  const [toastIsOpen, setToastIsOpen] = useState(false)
+  const [toastMessage, setToastMessage] = useState("")
+  const handleRegister = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    const firstName = formData.get("firstName")!.toString()
+    const lastName = formData.get("lastName")!.toString()
+    const phone = formData.get("phone")!.toString()
+    const birth = formData.get("birth")!.toString()
+    const email = formData.get("email")!.toString()
+    const password = formData.get("password")!.toString()
+    const confirmPassword = formData.get("confirmPassword")!.toString()
+    const params = { firstName, lastName, phone, birth, email, password}
+
+    if (password !== confirmPassword) {
+      setToastIsOpen(true)
+      setTimeout(() => {
+        setToastIsOpen(false)
+      }, 1000 * 3)
+      setToastMessage("Senha e confirmação diferentes.")
+      return
+    }
+
+    const { data, status } = await authService.register(params)
+
+    if (status === 201) {
+      router.push("/login?registred=true")
+    } else {
+      setToastIsOpen(true)
+      setTimeout(() => {
+        setToastIsOpen(false)
+      }, 1000 * 3)
+      setToastMessage(data.message)
+    }
+  }
+
   return (
     <>
       <Head>
@@ -22,7 +64,7 @@ const Register = () => {
           <p className={styles.formTitle}>
             Bem-vindo(a) ao OneBitFlix!
           </p>
-          <Form className={styles.form}>
+          <Form className={styles.form} onSubmit={handleRegister}>
             <p className="text-center">
               <strong>Faça a sua conta!</strong>
             </p>
@@ -115,8 +157,8 @@ const Register = () => {
                 CONFIRME SUA SENHA
               </Label>
               <Input
-                id="password"
-                name="password"
+                id="confirmPassword"
+                name="confirmPassword"
                 type="password"
                 placeholder="Confirme a sua senha"
                 required
@@ -135,6 +177,7 @@ const Register = () => {
           </Form>
         </Container>
         <Footer />
+        <ToastComponent color="bg-danger" isOpen={toastIsOpen} message={toastMessage} />
       </main>
     </>
   )
