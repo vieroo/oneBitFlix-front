8381f47f867bfd15923fbfb66f8cf6b34839b970
Eduardo Viero <duduviero@gmail.com>
2024-04-14 19:29:31 -0300

    finalizacao do search

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,6 +4,9 @@ import HeaderAuth from "@/src/components/common/headerAuth";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import courseService, { CourseType } from "@/src/services/courseService";
+import { Container } from "reactstrap";
+import SearchCard from "@/src/components/searchCard";
+import Footer from "@/src/components/common/footer/Footer";
 
 const Search = () => {
   const router = useRouter();
@@ -29,13 +32,24 @@ const Search = () => {
         <title>Onebitflix - {searchName}</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </Head>
-      <main>
-        <HeaderAuth />
-        {searchResult?.map((course) => (
-          <div key={course.id}>
-            <p>{course.name}</p>
-          </div>
-        ))}
+      <main className={styles.main}>
+        <div className={styles.headFooterBg}>
+          <HeaderAuth />
+        </div>
+        <section className={styles.mainContent}>
+          {searchResult.length >= 1 ? (
+            <Container className="d-flex flex-wrap justify-content-center gap-5 py-4">
+              {searchResult?.map((course) => (
+                <SearchCard key={course.id} course={course} />
+              ))}
+            </Container>
+          ) : (
+            <p className={styles.noSearchText}>Nenhum resultado encontrado!</p>
+          )}
+        </section>
+        <div className={styles.headFooterBg}>
+          <Footer />
+        </div>
       </main>
     </>
   );
