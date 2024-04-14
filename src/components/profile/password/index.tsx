9d54eeac3e94e8f2d4091646514bcc2448f00c20
Eduardo Viero <duduviero@gmail.com>
2024-04-14 10:20:52 -0300

    Pagina de alteração de senha

--- src/components/profile/password/index.tsx
+++ src/components/profile/password/index.tsx
@@ -1,10 +1,71 @@
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import styles from "../../../../styles/profile.module.scss";
+import { FormEvent, useEffect, useState } from "react";
+import profileService from "@/src/services/profileService";
+import ToastComponent from "../../common/toast";
 
 const PasswordForm = () => {
+  const [color, setColor] = useState("");
+  const [toastIsOpen, setToastIsOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState("");
+
+  useEffect(() => {
+    profileService.fetchCurrent().then((password) => {
+      setCurrentPassword(password.currentPassword);
+      setNewPassword(password.newPassword);
+    });
+  }, []);
+
+  const handlePasswordUpdate = async function (
+    event: FormEvent<HTMLFormElement>
+  ) {
+    event.preventDefault();
+
+    if (newPassword != confirmNewPassword) {
+      setToastIsOpen(true);
+      setErrorMessage("Senha e confirmação de senha diferentes!");
+      setColor("bg-danger");
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 * 3);
+
+      return;
+    }
+
+    const res = await profileService.passwordUpdate({
+      currentPassword,
+      newPassword,
+    });
+
+    if (res === 204) {
+      setToastIsOpen(true);
+      setErrorMessage("Senha alterada com sucesso!");
+      setColor("bg-success");
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 * 3);
+
+      setCurrentPassword("");
+      setNewPassword("");
+      setConfirmNewPassword("");
+    }
+
+    if (res === 400) {
+      setToastIsOpen(true);
+      setErrorMessage("Senha atual incorreta!");
+      setColor("bg-danger");
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 * 3);
+    }
+  };
+
   return (
     <>
-      <Form className={styles.form}>
+      <Form onSubmit={handlePasswordUpdate} className={styles.form}>
         <div className={styles.inputNormalDiv}>
           <FormGroup>
             <Label className={styles.label} for="currentPassword">
@@ -18,6 +79,10 @@ const PasswordForm = () => {
               required
               minLength={6}
               maxLength={12}
+              value={currentPassword}
+              onChange={(event) => {
+                setCurrentPassword(event.currentTarget.value);
+              }}
               className={styles.input}
             />
           </FormGroup>
@@ -35,6 +100,10 @@ const PasswordForm = () => {
               required
               minLength={6}
               maxLength={12}
+              value={newPassword}
+              onChange={(event) => {
+                setNewPassword(event.currentTarget.value);
+              }}
               className={styles.inputFlex}
             />
           </FormGroup>
@@ -50,6 +119,10 @@ const PasswordForm = () => {
               required
               minLength={6}
               maxLength={12}
+              value={confirmNewPassword}
+              onChange={(event) => {
+                setConfirmNewPassword(event.currentTarget.value);
+              }}
               className={styles.inputFlex}
             />
           </FormGroup>
@@ -58,6 +131,11 @@ const PasswordForm = () => {
           Salvar Alterações
         </Button>
       </Form>
+      <ToastComponent
+        color={color}
+        isOpen={toastIsOpen}
+        message={errorMessage}
+      />
     </>
   );
 };
--- src/services/profileService.ts
+++ src/services/profileService.ts
@@ -1,45 +1,70 @@
-import api from "./api"
+import api from "./api";
 
 export interface UserParams {
-  firstName: string
-  lastName: string
-  phone: string
-  email: string
-  createdAt: string
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  createdAt: string;
+}
+
+export interface PasswordParams {
+  currentPassword: string;
+  newPassword: string;
 }
 
 const profileService = {
   fetchCurrent: async () => {
-    const token = sessionStorage.getItem("onebitflix-token")
+    const token = sessionStorage.getItem("onebitflix-token");
 
-    const res = await api.get("/users/current", {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-    .catch((error) => {
-      return error.response
-    })
+    const res = await api
+      .get("/users/current", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        return error.response;
+      });
 
-    return res.data
+    return res.data;
   },
 
   userUpdate: async (params: UserParams) => {
-    const token = sessionStorage.getItem("onebitflix-token")
+    const token = sessionStorage.getItem("onebitflix-token");
 
-    const res = await api.put("/users/current", params, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-    .catch((error) => {
-      if (error.response.status === 400 || error.response.status === 401) {
-        return error.response
-      }
-    })
+    const res = await api
+      .put("/users/current", params, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        if (error.response.status === 400 || error.response.status === 401) {
+          return error.response;
+        }
+      });
 
-    return res.status
-  }
-}
+    return res.status;
+  },
 
-export default profileService
+  passwordUpdate: async (params: PasswordParams) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .put("/users/current/password", params, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        if (error.response.status === 400 || error.response.status === 401) {
+          return error.response;
+        }
+      });
+
+    return res.status;
+  },
+};
+
+export default profileService;
